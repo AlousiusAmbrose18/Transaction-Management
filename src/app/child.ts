ea3cff5d689eb07ce6bebf42bb694ba605529a81
Alousius <115810625+AlousiusAmbrose18@users.noreply.github.com>
2023-05-31 13:27:27 +0530

    Delete src directory

--- src/app/child.ts
+++
@@ -1,7 +0,0 @@
-import { Parent } from "./parent";
-
-export class Child {
-    id:any;
-    parent = new Parent();
-    paidAmount:any;
-}
--- src/app/parent.ts
+++
@@ -1,7 +0,0 @@
-export class Parent {
-    id: any;
-    sender:any;
-    receiver:any;
-    totalAmount:any;
-    totalPaidAmount:any;
-}
